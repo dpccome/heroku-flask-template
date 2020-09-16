bea35bf197df914dfe1772143b9eec15f57c90df
Justin Donnaruma <justin@donnaruma.us>
2020-09-16 13:38:22 -0400

    Add static img directory and token authentication

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
+import os
 from flask import Flask
 from flask_restx import Api, Resource, fields
+from flask_httpauth import HTTPTokenAuth
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Flask APIs for Heroku',
     description='A base to build Flask APIs and run them on the Heroku Platform.',
 )
+auth = HTTPTokenAuth(scheme='Bearer')
+
+APPNAME = os.getenv('HEROKU_APP_NAME')
+IMGDIR = f'https://{APPNAME}.herokuapp.com/static/img/' # Add image name and file extension as needed
+
+tokens = {
+    "token-one": os.getenv('BEARER_TOKEN')
+}
+
+@auth.verify_token
+def verify_token(token):
+    if token in tokens:
+        return tokens[token]
 
 PDFGENMODEL = api.model('PDFGen', {
     'template': fields.Integer,
@@ -33,14 +48,16 @@ GENPDF = PDFGen()
 
 @api.route('/pdfGen')
 class generatePDF(Resource):
-    @api.marshal_with(PDFGENMODEL)
+    @api.expect(PDFGENMODEL)
+    @auth.login_required
     def post(self):
         return GENPDF.generate(api.payload), 201
 
 
 @api.route('/pdfMerge')
 class mergePDF(Resource):
-    @api.marshal_with(PDFMERGEMODEL)
+    @api.expect(PDFMERGEMODEL)
+    @auth.login_required
     def post(self):
         return GENPDF.merge(api.payload), 201
 
